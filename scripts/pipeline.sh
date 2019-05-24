5ef06a2bd60adb2eef00958ec3a329614a629f44
Sipley <Breanna.Sipley@gmail.com>
2019-05-24 17:54:33 -0500

    Small edits to scripts

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,2 +1,12 @@
+# Make a tmp folder that will hold intermediate files & inter 'scripts' directory
+mkdir tmp
+cd scripts
+
+# Download query sequence of interest from GenBank
 python3 get_query.py > ../data/query.fasta
+
+# Download transcriptome(s)/genome(s) of interest from wormbase ParaSite
+# wormbase ParaSite (WBPS) is the largest, most current database of all things parasitic worms
 python3 get_transcriptome.py
+gunzip ../data/*gz
+
