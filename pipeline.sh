0952a821a59dc92fa37bdfc2cdbd754011f1340f
Sipley <Breanna.Sipley@gmail.com>
2019-05-26 06:17:45 -0500

    BLAST+ script successfully added

--- pipeline.sh
+++ pipeline.sh
@@ -1,36 +1,41 @@
 # Make a data & tmp folder that will hold retrieved data & intermediate files
 # Enter 'scripts' directory
-mkdir tmp
-mkdir data
+# mkdir tmp
+# mkdir data
 cd scripts
 
-# Download required databases
-python3 get_databases.py
-if ls ../tmp/*gz &>/dev/null
+# # Download required databases
+# python3 get_databases.py
+# if ls ../tmp/*gz &>/dev/null
+# then
+# 	gunzip ../tmp/*
+# 	echo 'Done getting current versions of required databases'
+# else
+# 	echo 'There was an issue retrieving required databases. Exiting...'
+# 	exit 1
+# fi
+
+# # Download query sequence of interest from GenBank
+# python3 get_query.py
+
+# # Download transcriptome(s)/genome(s) of interest from wormbase ParaSite
+# # wormbase ParaSite (WBPS) is the largest, most current database of all things parasitic worms
+# python3 get_transcriptome.py
+# if ls ../data/*gz &>/dev/null
+# then
+# 	gunzip ../data/*
+# 	echo 'Done getting transcriptome of interest'
+# else
+# 	echo 'There was an issue retrieving transcriptome. Exiting...'
+# 	exit 1
+# fi
+
+# BLAST query against transcriptomes(s)/genome(s) of interest
+bash blast.sh
+FILE=../tmp/potential_VAPs.fasta
+if [ -f "$FILE" ]
 then
-	gunzip ../tmp/*
-	echo 'Done getting current versions of required databases'
+	echo 'BLAST+ successfully completed'
 else
-	echo 'There was an issue retrieving required databases. Exiting...'
-	exit 1
+	echo 'There was an issue with BLAST+'
 fi
-
-# Download query sequence of interest from GenBank
-python3 get_query.py
-
-# Download transcriptome(s)/genome(s) of interest from wormbase ParaSite
-# wormbase ParaSite (WBPS) is the largest, most current database of all things parasitic worms
-python3 get_transcriptome.py
-if ls ../data/*gz &>/dev/null
-then
-	gunzip ../data/*
-	echo 'Done getting transcriptome of interest'
-else
-	echo 'There was an issue retrieving transcriptome. Exiting...'
-	exit 1
-fi
-
-# # BLAST query against transcriptomes(s)/genome(s) of interest
-# bash blast.sh
-# tidy=`awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }'`
-# $tidy tmp/blast_hits.fasta > tmp/potential_VAPs.fasta
--- scripts/blast.sh
+++ scripts/blast.sh
@@ -3,10 +3,13 @@ cd ../
 # make BLAST+ database (what you want to BLAST+ your query to)
 # BLAST+ your query against your BLAST+ database
 makeblastdb -in data/*fa -parse_seqids -dbtype nucl -out tmp/blast.db
-tblastn -query data/query.fasta -db tmp/blast.db -evalue 1e-3 -out tmp/blast.tblastn -num_threads 6 -outfmt 6
+tblastn -query data/query*fasta -db tmp/blast.db -evalue 1e-3 -out tmp/blast.tblastn -num_threads 6 -outfmt 6
 
 # Use BLAST+ output to determine IDs of BLAST+ hits
 # Pull out the sequences for your BLAST+ hits
 # Tidy up FASTA file output (wrap seq lines)
 awk -F "\t" '{print $2}' tmp/blast.tblastn | sort | uniq > tmp/contigLure.txt
 perl scripts/select_contigs.pl -n tmp/contigLure.txt data/*.fa tmp/blast_hits.fasta
+awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' tmp/blast_hits.fasta > tmp/potential_VAPs.fasta
+
+exit 0
