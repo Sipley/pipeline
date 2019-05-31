adb85f0b13000750e4763bd16f08446f98489921
Sipley <Breanna.Sipley@gmail.com>
2019-05-31 11:34:23 -0500

    Updating 'validate.sh' to print error message if any problematic VAPs

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -12,7 +12,13 @@ sed -i '/^$/d' transdecoder-complete_VAPs.pep
 # compare domain hits with transdecoder hits
 grep "CAP" pfam-validate_VAPs.domtblout | grep "Cysteine-rich secretory protein family" | awk '{print $4}' | sort | uniq > pfam-CRISP_names.txt 
 sed 's/\ .*//' transdecoder-complete_VAPs.pep | grep ">" | sed 's/>//' | sort | uniq > transdecoder-VAP_names.txt 
-diff transdecoder-VAP_names.txt pfam-CRISP_names.txt 
+diff transdecoder-VAP_names.txt pfam-CRISP_names.txt > problematic-VAPs.txt
+if [ -s /tmp/problematic-VAPs.txt ]
+then
+	echo "Looks like some VAPs may need a closer look. Please check 'problematic-VAPs.txt' and remove un-validated VAPs from 'transdecoder-comlete_VAPs.pep' before continuing."
+	exit 1
+else
+	echo "No problems here! All VAPs successfully validated."
 wc -l *names.txt
 
 
